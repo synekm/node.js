c60b8a7bddbadd6c29fa18b9fe3ad6fdb19ffaa6
Martin Synek <synekm@spstrutnov.cz>
2021-03-22 17:09:48 +0100

    Added express framework.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-const server = http.createServer(function(http_dotaz, http_odpoved) {
-    http_odpoved.write("Hello World!");
-    http_odpoved.end();
+const server = express();
+
+server.get('/', function(dotaz, odpoved) {
+    odpoved.send("Hello Express!");
 });
 
 server.listen(8000);
